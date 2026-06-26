58fefb3b4c2dea02a588d59a6f2f2a142479e83b
adad22ro <gabirusu2000@gmail.com>
2026-06-26 15:26:53 +0300

    Downgrade Next.js 15, configurare middleware Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import {ClerkProvider} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
